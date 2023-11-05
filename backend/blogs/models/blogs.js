64089c7418e702a5f79a525a2b4b0a6ae642da15
Nishchay Rajput <nishchayr@iitbhilai.ac.in>
2023-11-05 20:59:17 +0530

    Feat : blogs-hero data

--- backend/blogs/models/blogs.js
+++ backend/blogs/models/blogs.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 const userInfo = require('../../ecommerce/models/userInfo');
 
-
+// Define the schema for the 'blogs' collection
 const blogs = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId,ref : 'userInfo',  required: true },
-    // user_id : {type : String, required : true},
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    tags : {type : String},
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'userInfo', // Reference to the 'userInfo' model
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: String
+    },
+    like: {
+        type: Number,
+        default: 0 // Default value for 'like' field is 0
+    },
     time: {
         type: Date,
         default: function () {
@@ -19,4 +34,5 @@ const blogs = new mongoose.Schema({
     }
 });
 
+// Create and export the 'blogs' model based on the schema
 module.exports = mongoose.model('blogs', blogs);
