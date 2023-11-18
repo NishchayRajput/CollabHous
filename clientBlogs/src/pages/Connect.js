f1872e3d422fcc80139b6bce5e3365d17457694a
bhoomigoyal <bhoomig@iitbhilai.ac.in>
2023-11-18 22:53:31 +0530

    feat:last commit

--- clientBlogs/src/pages/Connect.js
+++ clientBlogs/src/pages/Connect.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "../components/Header";
+import "./css/Connect.css";
 
 const Connect = () => {
   return (
+    <div>
     <div style={{ minHeight: "100vh", backgroundColor: "rgba(35, 36, 38, 1)" }}>
   
       Connect Page
+    </div>
+    <div className="background-image">
+      </div>
+      <div className="button">
+      <button id="btn1">Connect</button>
+      <button id="btn2"> Job Openings</button>
+    </div>
+    <div>
+      <h1>cH</h1>
+    </div>
+
     </div>
   );
 };
