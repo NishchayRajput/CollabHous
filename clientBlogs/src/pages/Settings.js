16547ad2fd29300996c3d5497fd3536ecccc968c
sreyachitluru <chitlurus@iitbhilai.ac.in>
2023-11-19 00:05:47 +0530

    FIX:settings page fixed and settings card component created

--- clientBlogs/src/pages/Settings.js
+++ clientBlogs/src/pages/Settings.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import SettingsCard from "../components/SettingsCard";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
@@ -43,15 +44,9 @@ const Settings = ()=> {
                 <Card id="userBoard">
                     <CardMedia component="img" height="55%" image={image} alt="Paella dish" style={{backgroundColor:'pink'}} />
                     <Avatar id="avatar" sx={{ border: "2px solid rgba(35, 36, 38, 1)",boxShadow: 20}}></Avatar>
-                    <CardHeader 
-                        title=<p style={{
-                            color: "white",
-                            fontFamily: 'Roboto',
-                            fontWeight: "700",
-                            fontSize: "26px",
-                            lineHeight: "28px",
-                        }}>{username}</p>
-                        className="username"
+                    <CardHeader
+                        title=<p className="username">{username}</p>
+                        className="usernameHeader"
                     />
                     <CardContent>
                         <Typography className="bio">
@@ -94,6 +89,23 @@ const Settings = ()=> {
                                     />}
                             </Button>
                             </div>
+                            {socialMediaArrow===1? 
+                                <div className="socialMediaDrop">
+                                <div className="row">
+                                <Typography className="label">Instagram</Typography>
+                                <Typography className="values">Instagram</Typography>
+                                </div>
+                                <div className="row">
+                                <Typography className="label">Youtube</Typography>
+                                <Typography className="values">youtube</Typography>
+                                </div>
+                                <div className="row">
+                                <Typography className="label">Linkedin</Typography>
+                                <Typography className="values">linkedin</Typography>
+                                </div>
+                                </div> : ""
+                            }
+                            
                             <div className="row">
                                 <Typography className="label">Data & Privacy</Typography>
                                 <Button onClick={handleButtonDataClick} >
@@ -111,25 +123,42 @@ const Settings = ()=> {
                                     />}
                                 </Button>
                             </div>
-                            <div className="signout">
-                                <Button style={{
+                            {dataArrow===1?
+                                <div className="dataDrop">
+                                    <SettingsCard 
+                                        heading="Request my data"
+                                        content="Get a copy of the recruitment-related personal data that we process about you"
+                                        button="Request"
+                                    />
+                                    <SettingsCard 
+                                        heading="Remove my data"
+                                        content="We will assess your request and delete all your personal data we no longer have a reason to keep"
+                                        button="Request"
 
-                                    fontFamily: 'Roboto',
-                                    fontWeight: "700",
-                                    fontSize: "16px",
-                                    lineHeight: "19px",
-                                    color: "#545454",
-
-                                }}>
+                                    />
+                                    <SettingsCard 
+                                        heading="Privacy Policy"
+                                        content="Read more about how CHcommune collects and processes your personal data"
+                                        button="View"
+                                    />
+                                    <SettingsCard 
+                                        heading="Cookie Policy"
+                                        content="Find out more about how we use cookies to improve your user experience our website "
+                                        button="View"
+                                    />
+                                </div>
+                                : ""
+                            }
+                            <div className="signoutDiv">
+                                <Button className="signout">
                                     <img
-                                        src="images/VectorSignOut.png"
-                                        alt=""
-                                        style={{ marginLeft: "24px" }}
-
-                                    /><span >&nbsp;Signout</span> 
+                                    src="images/VectorSignOut.png"
+                                    alt=""/>
+                                        
+                                    <span >&nbsp;Signout</span> 
                                 </Button>
                             </div>
-                            
+                           
                         </div>
                 </Card>
             </div>
