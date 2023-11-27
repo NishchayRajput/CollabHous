b9e143d6b26ae342d41a8c816e73d0df75eaa0a1
Nishchay Rajput <nishchayr@iitbhilai.ac.in>
2023-11-27 21:19:46 +0530

    Feat : commune settings and backend logic

---
+++ backend/blogs/models/commune.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const userInfo = require('../../ecommerce/models/userInfo');
+
+const SettingsSchema = new mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true },
+    number: { type: String, required: true, maxLength: 10 },
+    primary_role: { type: String, default: '' }, // Provide default values as needed
+    job_notification_status: { type: String, default: '' },
+    job_notification_type: { type: String, default: '' },
+    image: { type: Buffer },
+});
+
+const QuestionSchema = new mongoose.Schema({
+    fname: { type: String, default: '' },
+    lname: { type: String, default: '' },
+    number: { type: String, maxLength: 10, default: '' },
+    pitch: { type: String, maxLength: 140, default: '' },
+    pdf: { type: Buffer },
+});
+
+const CommuneSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'userInfo' },
+    settings: SettingsSchema,
+    questions: [QuestionSchema],
+});
+
+// Use the singular form 'Commune' for the model name
+module.exports = mongoose.model('Commune', CommuneSchema);
