4e0eec01744df92b31437201957581c8b3ddda20
piyush-c38 <chandrakarpiyush36@gmail.com>
2023-12-16 00:19:41 +0530

    FIX: fixed the responsiveness of blog card, welcome commune page

--- clientBlogs/src/components/Hamburger.js
+++ clientBlogs/src/components/Hamburger.js
@@ -2,8 +2,31 @@ import React from "react";
 import "./css/Hamburger.css";
 
 import { slide as Menu } from "react-burger-menu";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Hamburger = () => {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    async function logout() {
+      try {
+        const { data } = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/blogs/logout`,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+        console.log(data);
+        if (data.message === "logout successfull") navigate("/home");
+        window.location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    logout();
+  };
   return (
     <div id="outer-container">
       <Menu
@@ -29,11 +52,15 @@ const Hamburger = () => {
             Connect
           </a>
           <div className="break-line" />
-          <a id="settings" className="menu-item" href="/settings">
+          <a id="profile" className="menu-item" href="/settings">
+            Profile
+          </a>
+          <div className="break-line" />
+          <a id="settings" className="menu-item" href="/setting">
             Settings
           </a>
           <div className="break-line" />
-          <a id="connect" className="menu-item" href="/connect">
+          <a id="connect" className="menu-item" onClick={handleLogOut}>
             LogOut
           </a>
         </main>
