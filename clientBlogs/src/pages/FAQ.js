157f3b9db53c526fe58d767e80cb7c2f719e0f88
bhoomigoyal <bhoomig@iitbhilai.ac.in>
2023-12-16 17:38:26 +0530

    Fix: FAQ page

--- clientBlogs/src/pages/FAQ.js
+++ clientBlogs/src/pages/FAQ.js
@@ -28,8 +28,8 @@ export default function BasicAccordion() {
       <div className="FAQcontainer">
         <div className="FAQtext">Frequently Asked Questions</div>
         <div className="FAQboxes">
-          <div><FAQBox /></div>
-          <div><FAQBox /></div>
+          <FAQBox />
+          <FAQBox />
           <FAQBox />
           <FAQBox />
           <FAQBox />
